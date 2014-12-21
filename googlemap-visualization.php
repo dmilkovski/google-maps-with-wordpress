7f289981bc941912dd102e14e4ce6901c44e40b5
Dimo <dimo@Debian.Debian-D>
2014-12-22 01:41:09 +0200

    Add custom post type to represent a point

--- googlemap-visualization.php
+++ googlemap-visualization.php
@@ -16,28 +16,37 @@ class GoogleMapVisualization {
     add_action ('init', array($this, 'register_gpm_post'));
     add_action ('init', array($this, 'create_gpm_taxonomies'));
   }
-  
+	function parser () {
+	}
   //register data types in WP
   public function register_gpm_post() {
 	  $labels = array(
-		  
+	      'name' => _x('Places'),
+	      'singular_name' => _x('Place'),
+	      'add_new' => __('Add new'),
+	      'all_items' => __('View all'),
+	      'menu_name' => 'Places',
+	      'not_found' => 'Place not found.'
 	  );
 
 	  $args = array(
-		  
+	      'labels' => $labels,
+	      'public' => true,
+	      'exclude_from_search' => true,
+	      'supports' => array('custom-fields', 'title', 'editor'),
 	  );
-
+		
 	  register_post_type( 'place', $args );
   }
   
   public function create_gpm_taxonomies() {
 	// Add new taxonomy, make it hierarchical (like categories)
 	$labels = array(
-		
+	    
 	);
 
 	$args = array(
-		
+	    
 	);
 
 	register_taxonomy( 'place_type', array( 'place' ), $args );
