13f46480aa8f1a9e02630ee1d3461d89dd8f81fe
Dimo <dimo@Debian.Debian-D>
2014-12-25 00:52:15 +0200

    Added working custom Fields Latitude and Longitude saved data on the database

--- googlemap-visualization.php
+++ googlemap-visualization.php
@@ -15,48 +15,37 @@ class GoogleMapVisualization {
     //init data
     add_action ('init', array($this, 'register_gpm_post'));
     add_action ('init', array($this, 'create_gpm_taxonomies'));
-    add_action ();
-    
-  }
 
-  public function register_custom_fileds () {
-  	add_meta_box("latitude_meta", "Latitude", "latitude_filed", "place", "normal", "high");
-  	add_meta_box("longitude_meta", "Longitude", "longitude_filed", "place", "normal", "high");
-  }
-
-  public function latitude_filed () {
-
-  }
-
-  public function longitude_filed () {
-
+    add_action ('admin_init', array ($this, 'register_custom_fileds'));
+    add_action ('save_post', array ($this, 'save_details'));
   }
 
   //register data types in WP
   public function register_gpm_post() {
-	  $labels = array(
-	      'name' => _x('Places'),
-	      'singular_name' => _x('Place'),
-	      'add_new_item' => __('Add new place item'),
-	      'add_new' => __('Add new'),
-	      'edit_item' => __('Edit place Item'),
-	      'menu_name' => __('Places'),
-	      'not_found' => __('Place not found.'),
-	      'not_found_in_trash' => __('Nothing found in trash.')
-	  );
+	$labels = array(
+	    'name' => _x('Places'),
+	    'singular_name' => _x('Place'),
+	    'add_new_item' => __('Add new place item'),
+	    'add_new' => __('Add new'),
+	    'edit_item' => __('Edit place Item'),
+	    'menu_name' => __('Places'),
+	    'not_found' => __('Place not found.'),
+	    'not_found_in_trash' => __('Nothing found in trash.')
+	);
 
-	  $args = array(
-	      'labels' => $labels,
-	      'public' => true,
-	      'publicly_queryable' => true,
-	      'query_var' => true,
-	      'capability_type' => 'post',
-	      'hierarchical' => false,
-	      'exclude_from_search' => true,
-	      'supports' => array('title', 'editor')
-	  );
+	$args = array(
+	    'labels' => $labels,
+	    'public' => true,
+	    'publicly_queryable' => true,
+	    'show_ui' => true,
+	    'query_var' => true,
+	    'capability_type' => 'post',
+	    'hierarchical' => false,
+	    'exclude_from_search' => true,
+	    'supports' => array('title', 'editor')
+	);
 		
-	  register_post_type( 'place', $args );
+	register_post_type( 'place', $args );
   }
   
   public function create_gpm_taxonomies() {
@@ -68,6 +57,39 @@ class GoogleMapVisualization {
 	);
 
 	register_taxonomy( 'place_type', array( 'place' ), $args );
+  }
+
+  public function register_custom_fileds () {
+  	add_meta_box("latitude_meta", "Latitude", array ($this, "latitude_filed"), "place", "normal", "high");
+  	add_meta_box("longitude_meta", "Longitude", array ($this, "longitude_filed"), "place", "normal", "high");
+  }
+
+  function latitude_filed () {
+  	global $post;
+  	$latitude_f = get_post_custom ($post->ID);
+  	$latitude = $latitude_f["latitude_filed"][0];
+  	?>
+  	<label>Latitude</label>
+  	<input type="text" name="latitude_filed" value="<?php echo $latitude; ?>" />
+  	<?php
+  }
+
+  function longitude_filed () {
+  	global $post;
+  	$longitude_f = get_post_custom ($post->ID);
+  	$longitude = $longitude_f["longitude_filed"][0];
+  	?>
+  	<label>Longitude</label>
+  	<input type="text" name="longitude_filed" value="<?php echo $longitude; ?>" />
+  	<?php
+  }
+
+  function save_details(){
+	  global $post;
+	 
+	  update_post_meta($post->ID, "latitude_filed", $_POST["latitude_filed"]);
+	  update_post_meta($post->ID, "longitude_filed", $_POST["longitude_filed"]);
+
   }
 
   public function map () {
